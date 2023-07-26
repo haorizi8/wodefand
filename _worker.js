9e467af00c049419f7ad80320c28fd497be1011c
haorizi8 <136498046+haorizi8@users.noreply.github.com>
2023-07-26 10:06:25 -0500

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = '9929881d-ryd0g0-rzt06b-rw2.hk.tcpbbr.net'
+      url.hostname = '420379a9-ryd0g0-rzt06b-rw2.fremont.comefromchinatown.com'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
